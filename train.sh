a027c20d2791c973d010e3c0dd73e58daa39d523
kylielee505 <161225403+kylielee505@users.noreply.github.com>
2024-11-01 07:18:29 +0530

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Activate the virtual environment
 source ./venv/bin/activate
+
+# Run the training script with the provided YAML configuration
 python3 ./src/train.py --yaml "$1"
+
+# Deactivate the virtual environment
 deactivate
