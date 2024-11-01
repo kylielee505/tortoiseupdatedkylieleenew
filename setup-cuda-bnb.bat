b569e4bcb901d547325bd2a154e12ef50ab87c1f
kylielee505 <161225403+kylielee505@users.noreply.github.com>
2024-11-01 07:02:48 +0530

    Update setup-cuda-bnb.bat

--- setup-cuda-bnb.bat
+++ setup-cuda-bnb.bat
@@ -1,6 +1,11 @@
+@echo off
 
+:: Clone the bitsandbytes-windows repository into the specified path
 git clone https://git.ecker.tech/mrq/bitsandbytes-windows.git .\modules\bitsandbytes-windows\
 
+:: Copy the necessary binaries and setup files into the virtual environment
 xcopy .\modules\bitsandbytes-windows\bin\* .\venv\Lib\site-packages\bitsandbytes\. /Y
 xcopy .\modules\bitsandbytes-windows\bin\cuda_setup\* .\venv\Lib\site-packages\bitsandbytes\cuda_setup\. /Y
 xcopy .\modules\bitsandbytes-windows\bin\nn\* .\venv\Lib\site-packages\bitsandbytes\nn\. /Y
+
+echo Setup complete.
