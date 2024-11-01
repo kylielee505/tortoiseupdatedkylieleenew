4f4f52a7d21fb99b48b1dcd5d5c6800987e3d3a0
kylielee505 <161225403+kylielee505@users.noreply.github.com>
2024-11-01 07:17:30 +0530

    Update train.bat

--- train.bat
+++ train.bat
@@ -1,5 +1,15 @@
+@echo off
+REM Activate the virtual environment
 call .\venv\Scripts\activate.bat
+
+REM Set the PYTHONUTF8 environment variable to handle UTF-8 encoding
 set PYTHONUTF8=1
+
+REM Run the training script with the provided YAML configuration
 python ./src/train.py --yaml "%1"
+
+REM Pause to keep the console window open
 pause
+
+REM Deactivate the virtual environment
 deactivate
