34493eda511be5ece25765a2d5f5abdd3a8bb242
kylielee505 <161225403+kylielee505@users.noreply.github.com>
2024-11-01 07:14:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+# Set the soft limit for the number of open file descriptors to the hard limit
 ulimit -Sn `ulimit -Hn` # ROCm is a bitch
+
+# Activate the Python virtual environment
 source ./venv/bin/activate
+
+# Execute the main Python script with all passed arguments
 python3 ./src/main.py "$@"
+
+# Deactivate the virtual environment
 deactivate
