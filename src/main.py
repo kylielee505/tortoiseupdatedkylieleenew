988534cf82335a3071297bb655cd53bb508dc18c
kylielee505 <161225403+kylielee505@users.noreply.github.com>
2024-11-01 06:47:59 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,40 +1,44 @@
 import os
+import sys
+import uvicorn
+from fastapi import FastAPI
+import gradio as gr
+from utils import setup_args, load_tts, setup_gradio
 
-if 'TORTOISE_MODELS_DIR' not in os.environ:
-	os.environ['TORTOISE_MODELS_DIR'] = os.path.realpath(os.path.join(os.getcwd(), './models/tortoise/'))
-
-if 'TRANSFORMERS_CACHE' not in os.environ:
-	os.environ['TRANSFORMERS_CACHE'] = os.path.realpath(os.path.join(os.getcwd(), './models/transformers/'))
-
+# Set up environment variables
+os.environ.setdefault('TORTOISE_MODELS_DIR', os.path.realpath(os.path.join(os.getcwd(), './models/tortoise/')))
+os.environ.setdefault('TRANSFORMERS_CACHE', os.path.realpath(os.path.join(os.getcwd(), './models/transformers/')))
 os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'
 
-from utils import *
-from webui import *
-
 if __name__ == "__main__":
-	args = setup_args()
+    # Ensure to reset sys.argv to prevent issues with Gradio
+    sys.argv = [sys.argv[0]]
+    args = setup_args()
 
-	if args.listen_path is not None and args.listen_path != "/":
-		import uvicorn
-		uvicorn.run("main:app", host=args.listen_host, port=args.listen_port if not None else 8000)
-	else:
-		webui = setup_gradio()
-		webui.launch(share=args.share, prevent_thread_lock=True, show_error=True, server_name=args.listen_host, server_port=args.listen_port)
-		if not args.defer_tts_load:
-			tts = load_tts()
+    # Check if listen_path is set
+    if args.listen_path is not None and args.listen_path != "/":
+        # Run FastAPI server with uvicorn
+        uvicorn.run("main:app", host=args.listen_host, port=args.listen_port if args.listen_port is not None else 8000)
+    else:
+        # Launch Gradio UI
+        webui = setup_gradio()
+        webui.launch(share=args.share, prevent_thread_lock=True, show_error=True, server_name=args.listen_host, server_port=args.listen_port)
+        
+        # Load TTS model if not deferred
+        if not args.defer_tts_load:
+            tts = load_tts()
+        
+        # Block the thread for Gradio
+        webui.block_thread()
 
-		webui.block_thread()
-elif __name__ == "main":
-	from fastapi import FastAPI
-	import gradio as gr
+# FastAPI application setup
+app = FastAPI()
 
-	import sys
-	sys.argv = [sys.argv[0]]
+# Add Gradio UI to FastAPI app if listen_path is not root
+if args.listen_path is not None and args.listen_path != "/":
+    webui = setup_gradio()
+    app = gr.mount_gradio_app(app, webui, path=args.listen_path)
 
-	app = FastAPI()
-	args = setup_args()
-	webui = setup_gradio()
-	app = gr.mount_gradio_app(app, webui, path=args.listen_path)
-
-	if not args.defer_tts_load:
-		tts = load_tts()
+# Load TTS model if not deferred
+if not args.defer_tts_load:
+    tts = load_tts()
