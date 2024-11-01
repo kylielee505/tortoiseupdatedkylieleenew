bc8ffc3da7cc5ec5d9917af59ec9bf74d10bb6a1
kylielee505 <161225403+kylielee505@users.noreply.github.com>
2024-11-01 07:08:18 +0530

    Update setup-docker.sh

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Initialize and update Git submodules
 git submodule init
 git submodule update --remote
+
+# Build the Docker image with the tag "ai-voice-cloning"
 docker build -t ai-voice-cloning .
