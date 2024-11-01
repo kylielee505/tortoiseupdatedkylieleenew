bbbde4db9179f2d743f1c6218eab0fad26680de8
kylielee505 <161225403+kylielee505@users.noreply.github.com>
2024-11-01 07:24:15 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+
+# Update the local Git repository
 git pull
+
+# Update all submodules to their latest commit
 git submodule update --remote
 
+# Activate the virtual environment
 source ./venv/bin/activate
-if python -m pip show whispercpp &>/dev/null; then python -m pip install -U git+https://git.ecker.tech/lightmare/whispercpp.py; fi
+
+# Check if whispercpp is installed and update it if it is
+if python -m pip show whispercpp &>/dev/null; then 
+    python -m pip install -U git+https://git.ecker.tech/lightmare/whispercpp.py
+fi
+
+# Deactivate the virtual environment
 deactivate
