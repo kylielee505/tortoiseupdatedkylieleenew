21a2b61c99cb5360946289ea70bcc5f847c54d7d
kylielee505 <161225403+kylielee505@users.noreply.github.com>
2024-11-01 07:13:13 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,15 @@
+@echo off
+REM Activate the Python virtual environment
 call .\venv\Scripts\activate.bat
+
+REM Prepend the local bin directory to the PATH
 set PATH=.\bin\;%PATH%
+
+REM Set the PYTHONUTF8 environment variable for UTF-8 support
 set PYTHONUTF8=1
+
+REM Run the main Python script with any provided arguments
 python .\src\main.py %*
+
+REM Pause the script to keep the window open after execution
 pause
